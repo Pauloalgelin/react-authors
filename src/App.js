c73a4f740b74256449d25aff83c387fa9460034c
Pauloalgelin <pauloalgelin@gmail.com>
2018-03-14 19:26:39 -0300

    Class 4.4: React Components, Props and CustomInput

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import CustomInput from './components/CustomInput'
 
 class App extends Component {
 
@@ -83,18 +84,9 @@ class App extends Component {
             <div className="content" id="content">
               <div className="pure-form pure-form-aligned">
                 <form className="pure-form pure-form-aligned" onSubmit={this.sendForm}>
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="name" value={this.state.name} onChange={this.setName}/>
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="passw" value={this.state.passw} onChange={this.setPassw}/>
-                  </div>
+                  <CustomInput htmlFor="name" label="Name" id="name" type="text" name="name" value={this.state.name} onChange={this.setName}/>
+                  <CustomInput htmlFor="email" label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
+                  <CustomInput htmlFor="passw" label="Password" id="passw" type="password" name="passw" value={this.state.passw} onChange={this.setPassw}/>
                   <div className="pure-control-group">
                     <label></label> 
                     <button type="submit" className="pure-button pure-button-primary">Gravar</button>
@@ -106,8 +98,8 @@ class App extends Component {
                 <table className="pure-table">
                   <thead>
                     <tr>
-                      <th>Nome</th>
-                      <th>email</th>
+                      <th>Name</th>
+                      <th>Email</th>
                     </tr>
                   </thead>
                   <tbody>
